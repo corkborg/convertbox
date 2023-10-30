ff212f3b191ef2a7e141a84405a5a14d6040bfca
corkborg <62742115+corkborg@users.noreply.github.com>
2023-10-30 19:27:24 +0900

    Fix test.

--- src/__tests__/index.test.jsx
+++ src/__tests__/index.test.jsx
@@ -1,12 +1,12 @@
 import {expect, jest, test} from '@jest/globals';
-import convert, { searchNumberEnd } from '@/service/convert_exponential'
+import convert, { trimZeroEnd } from '@/service/convert_exponential'
 
 describe('Home', () => {
-  it('searchNumberEnd', () => {
-    expect(searchNumberEnd('1111')).toBe('1111')
-    expect(searchNumberEnd('1111.000')).toBe('1111')
-    expect(searchNumberEnd('1111.00300')).toBe('1111.003')
-    expect(searchNumberEnd('1111.003')).toBe('1111.003')
-    expect(searchNumberEnd('1111.300')).toBe('1111.3')
+  it('trimZeroEnd', () => {
+    expect(trimZeroEnd('1111')).toBe('1111')
+    expect(trimZeroEnd('1111.000')).toBe('1111')
+    expect(trimZeroEnd('1111.00300')).toBe('1111.003')
+    expect(trimZeroEnd('1111.003')).toBe('1111.003')
+    expect(trimZeroEnd('1111.300')).toBe('1111.3')
   })
 })
