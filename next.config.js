945ef676c046c3776c240e06052828902e82e0fe
corkborg <62742115+corkborg@users.noreply.github.com>
2023-10-30 18:14:00 +0900

    Add gitworkflow.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const environment = process.env.NODE_ENV || development
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+}
+
+if(environment === 'production') {
+  nextConfig['basePath'] = '/convert-tools'
 }
 
 module.exports = nextConfig
