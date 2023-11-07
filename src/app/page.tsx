c9042471162c611852c39c76cb4c9070017226d0
corkborg <62742115+corkborg@users.noreply.github.com>
2023-11-07 14:42:18 +0900

    Fix toppage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,39 @@
 import Link from 'next/link'
 import MainTemplate from '@/components/main-template'
+import * as convert_exponential from '@/app/convert-exponential-notation/page'
+import * as beautify_json from '@/app/beautify-json/page'
+import * as sqllite_viewer from '@/app/viewer-sqlite/page'
+import * as decode_base64 from '@/app/decode-base64/page'
+import * as encode_base64 from '@/app/encode-base64/page'
+import Box from '@/components/organisms/box'
+
+type PageLinkProps = {
+  url: string,
+  pageData: {
+    metadata: {
+      title: string,
+      description?: string,
+    }
+  }
+}
+function PageLink({url, pageData}: PageLinkProps) {
+  return (
+    <Box title={<Link href={url}>{pageData.metadata.title}</Link>}>
+      {pageData.metadata.description && <p>{pageData.metadata.description}</p>}
+    </Box>
+  );
+}
 
 export default function Home() {
+
+
   return (
     <MainTemplate title="変換ツール一覧">
-      <Link href="convert-exponential-notation">convert-exponential-notation</Link>
-      <Link href="viewer-sqlite">viewer-sqlite</Link>
-      <Link href="beautify-json">beautify-json</Link>
-      <Link href="encode-base64">encode-base64</Link>
-      <Link href="decode-base64">decode-base64</Link>
+      <PageLink url="convert-exponential-notation" pageData={convert_exponential}></PageLink>
+      <PageLink url="viewer-sqlite" pageData={sqllite_viewer}></PageLink>
+      <PageLink url="beautify-json" pageData={beautify_json}></PageLink>
+      <PageLink url="encode-base64" pageData={encode_base64}></PageLink>
+      <PageLink url="decode-base64" pageData={decode_base64}></PageLink>
     </MainTemplate>
   )
 }
